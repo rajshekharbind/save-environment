d07ded81d1cd51efa6b7a605fec1c79130a0d57f
Your Name <raju.rajsekhar123@gmail.com>
2025-08-29 08:04:58 +0530

    add in useeffect section for proper working of darkmode

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -7,15 +7,14 @@ export const useDarkMode = () => {
     return saved ? JSON.parse(saved) : window.matchMedia('(prefers-color-scheme: dark)').matches
   })
 
-  // useEffect(() => {
-  //   localStorage.setItem('darkMode', JSON.stringify(isDark))
-  //   if (isDark) {
-
-  //     document.documentElement.classList.add('dark')
-  //   } else {
-  //     document.documentElement.classList.remove('dark')
-  //   }
-  // }, [isDark])
+  useEffect(() => {
+    localStorage.setItem('darkMode', JSON.stringify(isDark))
+    if (isDark) {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+  }, [isDark])
 
   const toggleDarkMode = () => setIsDark(!isDark)
 
