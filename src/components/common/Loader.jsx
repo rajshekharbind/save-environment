4e1a89b7953fcc60a7737525c4faf3d989481264
Your Name <raju.rajsekhar123@gmail.com>
2025-09-11 19:32:46 +0530

    commit

--- src/components/common/Loader.jsx
+++ src/components/common/Loader.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 import { motion } from 'framer-motion'
 
 const Loader = ({ size = 'medium', className = '' }) => {
-  const sizes = {
-    small: 'w-6 h-6',
-    medium: 'w-8 h-8',
-    large: 'w-12 h-12'
-  }
+  // const sizes = {
+  //   small: 'w-6 h-6',
+  //   medium: 'w-8 h-8',
+  //   large: 'w-12 h-12'
+  // }
 
   return (
     <div className={`flex items-center justify-center ${className}`}>
