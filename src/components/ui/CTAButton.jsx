f80bc5c2123945b9af5e5ef2115060ee4759236a
Your Name <raju.rajsekhar123@gmail.com>
2025-09-24 23:43:47 +0530

    update

--- src/components/ui/CTAButton.jsx
+++ src/components/ui/CTAButton.jsx
@@ -138,9 +138,9 @@ const EnhancedCTAButton = ({
           whileHover={{ x: 0 }}
           transition={{ duration: 0.28 }}
         >
-          <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          {/* <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M9 18L15 12L9 6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-          </svg>
+          </svg> */}
         </motion.span>
 
         {/* loading overlay */}
